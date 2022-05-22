11ae821383c789747ba0ee132ace4a8568d405f8
VipereSolide <68956220+VipereSolide@users.noreply.github.com>
2022-05-22 14:48:03 +0200

    Added a way to close the inspector.

--- Assets/Scripts/MenuWindow.cs
+++ Assets/Scripts/MenuWindow.cs
@@ -12,16 +12,30 @@ public class MenuWindow : MonoBehaviour
     [SerializeField] private RectTransform m_windowTransform;
 
     [Header("References")]
+    [SerializeField] private float m_slideLeftValue = -325f;
+    [SerializeField] private bool m_useTargetPosition;
+    [SerializeField] private Vector3 m_beforePosition;
+    [SerializeField] private Vector3 m_afterPosition;
+
+    [Space()]
+
     [SerializeField] private float m_shadowWindowFadeTime;
     [SerializeField] private float m_slideInWindowTime;
 
     public void SetActive(bool _Value)
     {
-        StartCoroutine(CanvasGroupHelper.Fade(m_shadowCanvasGroup, _Value, m_shadowWindowFadeTime));
+        if (m_shadowCanvasGroup != null) StartCoroutine(CanvasGroupHelper.Fade(m_shadowCanvasGroup, _Value, m_shadowWindowFadeTime));
 
-        Vector3 _startPos = new Vector3((_Value) ? -325f : 325f, m_windowTransform.position.y, m_windowTransform.position.z);
-        Vector3 _targetPos = new Vector3((_Value) ? 325f : -325f, m_windowTransform.position.y, m_windowTransform.position.z);
-        StartCoroutine(FadePos(m_windowTransform, _startPos, _targetPos, m_slideInWindowTime));
+        if (!m_useTargetPosition)
+        {
+            Vector3 _startPos = new Vector3((_Value) ? m_slideLeftValue : -m_slideLeftValue, m_windowTransform.position.y, m_windowTransform.position.z);
+            Vector3 _targetPos = new Vector3((_Value) ? -m_slideLeftValue : m_slideLeftValue, m_windowTransform.position.y, m_windowTransform.position.z);
+            StartCoroutine(FadePos(m_windowTransform, _startPos, _targetPos, m_slideInWindowTime));
+        }
+        else
+        {
+            StartCoroutine(FadePos(m_windowTransform, (_Value) ? m_beforePosition : m_afterPosition, (_Value) ? m_afterPosition : m_beforePosition, m_slideInWindowTime));
+        }
     }
 
     private IEnumerator FadePos(Transform _object, Vector3 _start, Vector3 _target, float overTime)
